cc7771f8211c8df6f72e3629067707c6d15e1f19
Yorth H. Ortegon <yorth@udenar.edu.co>
2025-07-21 00:35:30 -0500

    feat: agregar componente Toaster en el layout principal para notificaciones

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Toaster } from "@/components/ui/sonner";
 import AuthSessionProvider from "@/providers/session-provider";
 import "./globals.css";
 
@@ -29,6 +30,7 @@ export default function RootLayout({
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
 				<AuthSessionProvider>{children}</AuthSessionProvider>
+				<Toaster />
 			</body>
 		</html>
 	);
