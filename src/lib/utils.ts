d53cb6dfc04ceaf0fd1e37ec32c36900600a3b8f
Yorth H. Ortegon <yorth@udenar.edu.co>
2025-07-27 18:47:47 -0500

    feat: agregar función para convertir errores a cadenas de texto, mejorando la gestión de errores en la aplicación

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,16 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
+
+export function errorToString(
+	err: unknown,
+	fallback: string = "Ocurrió un error inesperado",
+): string {
+	if (typeof err === "string") return err;
+	if (err instanceof Error) return err.message;
+	try {
+		return JSON.stringify(err);
+	} catch {
+		return fallback;
+	}
+}
