1e0fc87eacd7b3e129683fbe8ba193441872b693
Yorth H. Ortegon <yorth@udenar.edu.co>
2025-07-31 01:21:00 -0500

    feat: agregar manejo de errores en la búsqueda de salidas de estacionamiento y nueva función para actualizar el estado de salida, mejorando la robustez y funcionalidad del sistema

--- src/lib/repositories/parking/parking-exit.ts
+++ src/lib/repositories/parking/parking-exit.ts
@@ -1,7 +1,10 @@
 import { eq } from "drizzle-orm";
 import { db } from "@/db";
 import { parkingExit } from "@/db/schema";
-import type { ParkingExit } from "@/lib/types/parking-schema";
+import type {
+	ParkingExit,
+	ParkingExitStatus,
+} from "@/lib/types/parking-schema";
 
 export async function createParkingExit(exit: Omit<ParkingExit, "id">) {
 	try {
@@ -19,7 +22,15 @@ export async function findParkingExitById(id: number) {
 			.from(parkingExit)
 			.where(eq(parkingExit.id, id))
 			.limit(1);
-		return { ok: true, data: exit || null };
+
+		if (!exit) {
+			return {
+				ok: false,
+				error: `No se encontró la salida para el ID ${id}`,
+			};
+		}
+
+		return { ok: true, data: exit };
 	} catch (err: unknown) {
 		return { ok: false, error: err };
 	}
@@ -33,8 +44,32 @@ export async function findParkingExitByEntryId(entryId: number) {
 			.from(parkingExit)
 			.where(eq(parkingExit.entryId, entryId))
 			.limit(1);
-		return { ok: true, data: exit || null };
+
+		if (!exit) {
+			return {
+				ok: false,
+				error: `No se encontró la salida para el ID de entrada ${entryId}`,
+			};
+		}
+		return { ok: true, data: exit };
 	} catch (err: unknown) {
 		return { ok: false, error: err };
+	}
+}
+
+export async function updateParkingExitStatus(
+	id: number,
+	status: ParkingExitStatus,
+) {
+	try {
+		await db
+			.update(parkingExit)
+			.set({ status })
+			.where(eq(parkingExit.id, id))
+			.returning();
+	} catch (err: unknown) {
+		throw new Error(
+			`Error al actualizar el estado de la salida ${id} a ${status}: ${err}`,
+		);
 	}
 }
